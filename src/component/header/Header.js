dd3a9843f79de6937cb8512edc47542aed0a4be5
sadiqss <sadiq.sau48@gmail.com>
2022-06-11 00:22:33 +0100

    header edited

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -14,22 +14,8 @@ const Header = () => {
                 <h5 className="text-light">
                     <TextAnimation></TextAnimation>
                 </h5>
-
-
                 <CTA></CTA>
-
                 <HeaderSocial></HeaderSocial>
-
-                <div className='img_des'>
-                    <div className="me">
-                        <img src={me} alt="" />
-                    </div>
-                    <div className='head_description'>
-                        <p>Rahman is a seasoned full-stack developer and software architect with in-depth knowledge of Node.js, and React. He has a unique ability of understanding and translating business requirements into development roadmaps.</p>
-                    </div>
-                </div>
-
-                <a href="#contact" className='scroll-down'>Scroll Down</a>
             </div>
         </header>
     );
